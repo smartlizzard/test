b63bdee17e8f713057c4e692d067083a9c218fc7
smartlizzard <32848463+smartlizzard@users.noreply.github.com>
2020-01-18 13:13:06 +0530

    Create testFile.groovy

---
+++ testFile.groovy
@@ -0,0 +1,59 @@
+#!/usr/bin/env groovy
+
+def GitBranch
+
+pipeline {
+    agent any
+
+    stages {
+        stage('CleanWorkspace') {
+            steps {
+                cleanWs()
+            }
+        }
+
+        stage('Cloning BuildCode') {
+          steps {
+                GitBranch = "${GIT_BRANCH}"
+                echo "${GitBranch}"
+                git branch: "${GitBranch}",
+                credentialsId: 'GitHub',
+                url: "https://github.com/smartlizzard/test.git"
+            }
+        }
+        
+        stage('Test') {
+            steps {
+                script {
+                    def files = readFile('./tags.properties').readLines()
+                        for (int i = 1; i <= files.size(); i++) {
+                            echo "Number of IP : $i" 
+                            sh "echo \"PUB$i=\$(aws ec2 describe-instances --filter \"Name=tag:Name,Values=`head -n$i ./tags.properties | tail -1`\"  --region us-east-1  | jq .Reservations[].Instances[].NetworkInterfaces[].PrivateIpAddress)\" >> file.propertise"
+                        }
+                    sh "cat file.propertise"    
+                }
+            }
+        }
+        stage('Test1') {
+            steps {
+                script {
+                    def files = readFile('./tags.properties').readLines()
+                        for (int i = 1; i <= files.size(); i++) {
+                            echo "Number of IP : $i"
+                            sh "echo [PUB$i] >> file2.propertise"
+                            sh "echo \"\$(aws ec2 describe-instances --filter \"Name=tag:Name,Values=`head -n$i ./tags.properties | tail -1`\"  --region us-east-1  | jq -r .Reservations[].Instances[].NetworkInterfaces[].PrivateIpAddress)\" >> file2.propertise"
+                        }
+                    sh "cat file2.propertise"    
+                }
+            }
+        }
+        stage('Test2') {
+            steps {
+                script {
+                    load "file.propertise"
+                    echo "${PUB1}"
+                }
+            }
+        }
+    }
+}
