8c89292abe11df0e5f16467036e8c6844aea3dec
smartlizzard <pradhanraghunath91@gmail.com>
2020-02-13 01:03:06 +0530

    update

--- conditionalSteps.groovy
+++ conditionalSteps.groovy
@@ -33,6 +33,7 @@ pipeline {
         stage('CREATE_INVENTORY') {
             steps {
                 script {
+                    sh "ls -lrth"
                     sh "sh script.sh ${params.PUBLISHER}"
                     sh 'cat inventory.ini'
                 }
