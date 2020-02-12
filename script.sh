45cea7c6170a3fc56249d8819f73fae24ca3e882
smartlizzard <pradhanraghunath91@gmail.com>
2020-02-13 01:06:09 +0530

    update

--- script.sh
+++ script.sh
@@ -1,13 +1,12 @@
-publisher=$1
 if ! grep -E "RestDispstcher" inventory.ini; then 
  sed -i "/[Dispatcher]/a[RestDispstcher]" inventory.ini
 fi
-lines=$(grep PUB tags.properties | wc -l)
-line=1
+export lines=$(grep PUB tags.properties | wc -l)
+export line=1
 while (( $line <= $lines ))
 do  
     DISPIP=$(grep DISP$line= disp.properties | sed "s/DISP$line=//g" | sed 's/"//g' )
-    if [ "$publisher" == "PUB$line" ]; then
+    if [ "$1" == "PUB$line" ]; then
         sed -i "/\[Dispatcher\]/a$DISPIP" inventory.ini
     else 
         sed -i "/\[RestDispstcher\]/a$DISPIP" inventory.ini
