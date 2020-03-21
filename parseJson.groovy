603f5a09c8d6183d5564dc9607a3d00b7c4e817a
smartlizzard <pradhanraghunath91@gmail.com>
2020-03-21 19:13:53 +0530

    update

--- parseJson.groovy
+++ parseJson.groovy
@@ -31,12 +31,9 @@ pipeline {
             steps {
                 script {
                     def props = readJSON file: 'properties.json'
-                    def count = assert props.Properties.Environment.Prod.Tags.Dispature.size()
-                    echo "count = $count"
-                    for (int i = 1; i <= count; i++) {
-                        for (key in props.Properties.Environment.Prod.Tags.Dispature.get(i)) {
-                            echo "key=${key}"
-                        }   
+                    assert props.Properties.Environment.Prod.Tags.Dispature.size()
+                    assert props.Properties.Environment.Prod.Tags.Dispature.each{
+                    println it 
                     }
                 }
             }
