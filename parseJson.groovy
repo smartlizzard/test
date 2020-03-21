699f9be78cced38a92a131ea02059f5d283f63bf
smartlizzard <pradhanraghunath91@gmail.com>
2020-03-21 18:16:12 +0530

    update

--- parseJson.groovy
+++ parseJson.groovy
@@ -31,10 +31,8 @@ pipeline {
             steps {
                 script {
                     def props = readJSON file: 'properties.json', returnPojo: true
-                    for (key in props.Properties.Environment.Dev.Tags.Dispature.get()) {
-                        echo "key=${key}"
-                        echo "value= ${props.Properties.Environment.Dev.Tags.Dispature.get(key)}"
-                    }
+                    def count = props.Properties.Environment.Dev.Tags.Dispature.size()
+                    echo "count = $count"
                 }
             }
         }
