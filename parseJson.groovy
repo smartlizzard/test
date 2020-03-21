c09701ab5f524995feea5a2cda4083ef12532e5b
smartlizzard <pradhanraghunath91@gmail.com>
2020-03-21 17:26:38 +0530

    update

---
+++ parseJson.groovy
@@ -0,0 +1,43 @@
+#!/usr/bin/env groovy
+
+def SOURCE_CODE_BRANCH = "${CODE_BRANCH}"
+
+pipeline {
+    agent any
+     parameters { 
+         string(name: 'CODE_BRANCH', defaultValue: 'master', description: 'Branch Name')
+     }
+
+    stages {
+        stage('CleanWorkspace') {
+            steps {
+                cleanWs()
+            }
+        }
+
+        stage('Checkout') {
+            steps {
+                script {
+                    def GIT_BRANCH_LOCAL = sh (script: "echo ${GIT_BRANCH} | sed -e 's|origin/||g'",returnStdout: true).trim()
+                    echo "${CODE_BRANCH}"
+                    echo "${GIT_BRANCH_LOCAL}"
+                    echo "${GIT_URL}"
+                    git branch: "${GIT_BRANCH_LOCAL}",
+                    credentialsId: 'GitHub',
+                    url: "${GIT_URL}"
+                }
+            }
+        }
+
+        stage('Test') {
+            steps {
+                script {
+                    def props = readJSON file: 'properties.json', returnPojo: true
+                    props.each { key, value ->
+                        echo "Walked through key $key and value $value"
+                    }
+                }
+            }
+        }
+    }
+}
--- testFile.groovy
+++ testFile.groovy
@@ -16,31 +16,32 @@ pipeline {
         }
 
         stage('Checkout') {
-          steps {
-              script {
-                  def GIT_BRANCH_LOCAL = sh (script: "echo ${GIT_BRANCH} | sed -e 's|origin/||g'",returnStdout: true).trim()
-                  echo "${CODE_BRANCH}"
-                  echo "${GIT_BRANCH_LOCAL}"
-                  echo "${GIT_URL}"
-                  git branch: "${GIT_BRANCH_LOCAL}",
-                      credentialsId: 'GitHub',
-                      url: "${GIT_URL}"
-                  }
-              }
+            steps {
+                script {
+                    def GIT_BRANCH_LOCAL = sh (script: "echo ${GIT_BRANCH} | sed -e 's|origin/||g'",returnStdout: true).trim()
+                    echo "${CODE_BRANCH}"
+                    echo "${GIT_BRANCH_LOCAL}"
+                    echo "${GIT_URL}"
+                    git branch: "${GIT_BRANCH_LOCAL}",
+                    credentialsId: 'GitHub',
+                    url: "${GIT_URL}"
+                }
             }
-        
+        }
+
         stage('Test') {
             steps {
                 script {
                     def files = readFile('./tags.properties').readLines()
-                        for (int i = 1; i <= files.size(); i++) {
-                            echo "Number of IP : $i" 
-                            sh "echo \"PUB$i=\$(aws ec2 describe-instances --filter \"Name=tag:Name,Values=`head -n$i ./tags.properties | tail -1`\"  --region us-east-1  | jq .Reservations[].Instances[].NetworkInterfaces[].PrivateIpAddress)\" >> file.propertise"
-                        }
-                    sh "cat file.propertise"    
+                    for (int i = 1; i <= files.size(); i++) {
+                        echo "Number of IP : $i" 
+                        sh "echo \"PUB$i=\$(aws ec2 describe-instances --filter \"Name=tag:Name,Values=`head -n$i ./tags.properties | tail -1`\"  --region us-east-1  | jq .Reservations[].Instances[].NetworkInterfaces[].PrivateIpAddress)\" >> file.propertise"
+                    }
+                    sh "cat file.propertise"
                 }
             }
         }
+
         stage('Test1') {
             steps {
                 script {
@@ -54,6 +55,7 @@ pipeline {
                 }
             }
         }
+        
         stage('Test2') {
             steps {
                 script {
