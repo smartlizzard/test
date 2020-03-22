623f191e426052305f0fe52ce57cc172708f2e49
smartlizzard <pradhanraghunath91@gmail.com>
2020-03-22 11:25:03 +0530

    update

--- parseJson.groovy
+++ parseJson.groovy
@@ -31,7 +31,7 @@ pipeline {
             steps {
                 script {
                     def props = readJSON file: 'properties.json'
-                    def count = props['Properties','Environment']
+                    def count = props['Properties']['Environment']
                     echo "count = $count"
                     /*
                     for (int i = 1; i <= count.lenth; i++) {
