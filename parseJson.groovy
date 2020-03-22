ffaef65ee2b9e59ae93b430acdea25cccbf8293b
smartlizzard <pradhanraghunath91@gmail.com>
2020-03-22 10:50:45 +0530

    update

--- parseJson.groovy
+++ parseJson.groovy
@@ -31,15 +31,13 @@ pipeline {
             steps {
                 script {
                     def props = readJSON file: 'properties.json'
-                    def count = props.Properties.Environment.Prod.Tags.Dispature
-                    echo "count = $count.count()"
-                    /*
-                    for (int i = 1; i <= count.lenth; i++) {
+                    def count = "cat properties.json | jq .Properties[].Environment[].Prod[].Tags[].Dispature[] | grep 'Pub' | wc -l".execute()
+                    echo "count = $count"
+                    for (int i = 1; i <= count; i++) {
                         for (key in props.Properties.Environment.Prod.Tags.Dispature.get(i)) {
                             echo "key=${key}"
                         }   
                     }
-                    */
                     /*
                     props.Properties.Environment.Prod.Tags.Dispature.each { key, value ->
                         echo "Walked through key $key and value $value"
